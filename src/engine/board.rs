f993107fcbd3b64d565196070f2dc7795de14d2e
oceona <emoceon@gmail.com>
2026-04-22 23:59:05 +0100

    feat: add some functions

--- src/engine/board.rs
+++ src/engine/board.rs
@@ -1,15 +1,30 @@
 use std::fmt;
-use std::fmt::{Debug, Formatter};
+use std::ops::{BitAnd, BitOr, BitXor, Not};
 
 #[derive(PartialEq, Eq, PartialOrd, Clone, Copy, Debug, Default, Hash)]
 pub struct BitBoard(pub u64);
 
 impl BitBoard {
     pub const EMPTY: BitBoard = BitBoard(0);
-}
+    pub const ALL: BitBoard = BitBoard(0xFFFFFFFFFFFFFFFF);
 
-// Check if square needs a piece is_set(square)
-// Add a piece set(square)
-// Remove a piece clear(square)
-// Count pieces count(piece)
-// Find all pieces - iterate over?
+    // Create the BitBoard
+    pub fn from_square(square: u8) -> Self {
+        debug_assert!(square < 64);
+        BitBoard(1u64 << square)
+    }
+
+    // Check if square needs a piece is_set(square)
+    pub fn is_set(&mut self, square: u8) -> bool {
+        (self.0 & (1u64 << square)) != 0
+    }
+
+    // Add a piece set(square)
+    pub fn set_bit(&mut self, square: u8) {
+        self.0 |= 1u64 << square;
+    }
+
+    // Remove a piece clear(square)
+    // Count pieces count(piece)
+    // Find all pieces - iterate over?
+}
