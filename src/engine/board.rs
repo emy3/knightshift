0b0f73896c9bf0a180af00fe4ef44f42af951f48
oceona <emoceon@gmail.com>
2026-04-27 00:13:29 +0100

    feat: visualize board, add pieces, update main

--- src/engine/board.rs
+++ src/engine/board.rs
@@ -6,7 +6,20 @@ pub struct BitBoard(pub u64);
 
 impl BitBoard {
     pub const EMPTY: BitBoard = BitBoard(0);
-    pub const ALL: BitBoard = BitBoard(0xFFFFFFFFFFFFFFFF);
+    //pub const ALL: BitBoard = BitBoard(0xFFFFFFFFFFFFFFFF);
+
+    // Display the BitBoard
+    pub fn print(&self) {
+        const LAST_BIT: u64 = 63;
+        for rank in 0..8 {
+            for file in (0..8).rev() {
+                let mask = 1u64 << (LAST_BIT - (rank * 8) - file);
+                let char = if self.0 & mask != 0 { '1' } else { '0' };
+                print!("{char}");
+            }
+            println!();
+        }
+    }
 
     // Create the BitBoard
     pub fn from_square(square: u8) -> Self {
---
+++ src/engine/piece.rs
@@ -0,0 +1,11 @@
+pub struct Pieces;
+
+impl Pieces {
+    pub const KING: Piece = 0;
+    pub const QUEEN: Piece = 1;
+    pub const ROOK: Piece = 2;
+    pub const BISHOP: Piece = 3;
+    pub const KNIGHT: Piece = 4;
+    pub const PAWN: Piece = 5;
+    pub const NONE: Piece = 6;
+}
